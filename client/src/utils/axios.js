4d8e24489e057f80b02458bf413b4a08d32867b6
Reshoo-18 <reshooranjan7@gmail.com>
2026-09-03 13:30:59 +0530

    backend url feed in frontend

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const productApi=async()=>{
     try {
-        const response= await axios.get("http://localhost:8000/products")
+        const response= await axios.get("https://mani-electricals.onrender.com/products")
         return response.data.products
     } catch (error) {
         console.log(error)
